93aafcb5a6cdb7ff88d4a27a1cb49a0807369892
johndward01 <johndward01@gmail.com>
2021-02-17 00:55:56 -0600

    initial commit

---
+++ ASP.NET_Teacher_Demo/ASP.NET_Teacher_Demo_ConsoleUI/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET_Teacher_Demo_ConsoleUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Teacher_Demo_ConsoleUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+
+        public ProductController(IProductRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var products = _repo.GetAllProducts();
+
+            return View(products);
+        }
+
+        public IActionResult ViewProduct(int id)
+        {
+            var product = _repo.GetProduct(id);
+
+            return View(product);
+        }
+
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = _repo.GetProduct(id);
+
+            _repo.UpdateProduct(prod);
+
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            _repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+
+        public IActionResult InsertProduct()
+        {
+            var prod = _repo.AssignCategory();
+
+            return View(prod);
+        }
+
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            _repo.InsertProduct(productToInsert);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            _repo.DeleteProduct(product);
+
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ ASP.NET_Teacher_Demo/ASP.NET_Teacher_Demo_ConsoleUI/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET_Teacher_Demo_ConsoleUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Teacher_Demo_ConsoleUI
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
